efc9c30152b1fb983b6fc87e8e737d1921ab3058
PRYtheSheep <122869360+PRYtheSheep@users.noreply.github.com>
2023-03-21 15:34:30 +0800

    Update DatabaseProjectAccessor.java

--- databaseProject/DatabaseProjectAccessor.java
+++ databaseProject/DatabaseProjectAccessor.java
@@ -1,5 +1,13 @@
 package databaseProject;
 
 public class DatabaseProjectAccessor {
-
+	public static void addProject(Project p) {
+		ProjectArray.projectArray.add(p);
+	}
+	
+	public static void updateProjectStatusInDatabase(int id, int status) {
+		Project p = ProjectArray.projectArray.get(id-1);
+		p.status = status;
+		ProjectArray.projectArray.set(id-1, p);
+	}
 }
