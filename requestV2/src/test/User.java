279f518aad173531f70c9ea48b20e82e1fd05460
OSA7JIMI <126651855+OSA7JIMI@users.noreply.github.com>
2023-03-21 20:25:48 +0800

    Add files via upload

--- requestV2/src/test/User.java
+++ requestV2/src/test/User.java
@@ -1,14 +1,16 @@
-package test;
+package sc2002_assignment;
 
 import java.util.Scanner;
-
 import request.Request;
+import java.util.LinkedList;
 
 public class User {
 	private String userID;
 	private String password;
 	private String name;
 	private String email;
+	private LinkedList <Request> incoming;
+	private LinkedList<Request> outgoing;
 	
 	User(String ID) {
 		userID = ID;
@@ -27,28 +29,29 @@ public class User {
 		return email;
 	}
 	
+	public String getPassword() {
+		return password;
+	}
+	
 	public void setPassword() {
 		Scanner a = new Scanner(System.in);
 		System.out.println("Please enter your new password: ");
 		password = a.nextLine();
 	}
-	//NEW
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+		
 	public void setEmail(String email) {
 		this.email = email;
-	}
+	}	
 	
-	 public void viewAllProjects() {
-	}
-	 public void viewAllRequests() {
-	}
-	 public void changeTitle() {
-	}
-	 public void displayOptions() {
-	}
-	 public void updateOutgoingRequest(int senderIndex, boolean approval) {
-	}
+	public void viewAllProjects() {;}
+	public void viewAllRequests() {;} 
+	public void changeTitle() {;}
+	public void displayOptions() {;}
+	public void updateIncomingRequest(int senderIndex, boolean approval) {;}
+	public void updateOutgoingRequest(int senderIndex, boolean approval) {;}
+	
 }
