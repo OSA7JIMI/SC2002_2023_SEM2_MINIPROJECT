27d14bf5a95da78856a6e6b26469d02938b59cc6
PRYtheSheep <122869360+PRYtheSheep@users.noreply.github.com>
2023-03-21 15:35:06 +0800

    Update ProjectArray.java

--- databaseProject/ProjectArray.java
+++ databaseProject/ProjectArray.java
@@ -1,5 +1,7 @@
 package databaseProject;
 
-public class ProjectArray {
+import java.util.ArrayList;
 
+public class ProjectArray {
+	static ArrayList<Project> projectArray = new ArrayList<Project>();
 }
