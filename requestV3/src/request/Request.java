00e0429cfdcce9e81dbe231441f59307eb899476
PRYtheSheep <122869360+PRYtheSheep@users.noreply.github.com>
2023-03-25 10:39:39 +0800

    Update Request.java

--- requestV3/src/request/Request.java
+++ requestV3/src/request/Request.java
@@ -5,7 +5,7 @@ import test.User;
 
 public class Request {
 	
-	public User sender;
+	public int requestIndex;
 	public String senderID;
 	public String receiverID;
 	public boolean pending;
@@ -13,7 +13,6 @@ public class Request {
 	//0 for changeTitle, 1 for Allocate, 2 for De-register, 3 for Transfer
 	public int type;
 	//contains the index position in the sender's outgoing request array
-	public int senderIndex;
 	public String Title;
 	public int projectID = -1;
 	
