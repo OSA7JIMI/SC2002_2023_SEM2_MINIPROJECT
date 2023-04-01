7fba66ddbd2c865955e9d9bce1bf21b21822d0ca
OSA7JIMI <126651855+OSA7JIMI@users.noreply.github.com>
2023-04-01 19:37:27 +0800

    Update FYP_App.java

--- 2002ASSIGNMENT/src/Test/FYP_App.java
+++ 2002ASSIGNMENT/src/Test/FYP_App.java
@@ -8,13 +8,10 @@ import databaseUser.databaseUserAccessor;
 public class FYP_App {
 
 	public static void main(String[] args) {
-		// To-do: create User objects from initialization file
-		// and add them to UserArray
-		boolean logout = false;
-		
-			UserArray.main(args);
+		boolean end = false;
+		UserArray.main(args);
 			
-		
+		while(!end){
 			System.out.println("Welcome to the FYP registration system!");
 			System.out.println("Please enter your user ID to log in: ");
 			Scanner scan = new Scanner(System.in);
@@ -25,33 +22,20 @@ public class FYP_App {
 			System.out.println("Please enter your password: ");
 			String password = scan.nextLine();
 
-			/*int max_tries=3;
-			for(int i =0; i<max_tries; i++) {
-				if(password != currentUser.getPassword()) {
-				System.out.println("Password is incorrect, "+ (max_tries-i)+ " tries remaining.");
-				password = scan.nextLine();
-				}
-			}*/
-			
-			// we should ask Li Fang about limiting password tries
-
 			while(!password.equals(currentUser.getPassword())) {
 				System.out.println("Password is incorrect, please try again");
 				password = scan.nextLine();
 			}
 
-			System.out.println("You have successfully logged in");
+			System.out.println("Welcome, currentUser.getName(), you have successfully logged in");
 
-			while(!logout) {
-				currentUser.displayOptions();
-				System.out.println("Enter /logout to log out of user account");
-				System.out.println("Press enter to continue");
-				String check = scan.nextLine();
-				if(check.equals("/logout")) {
-					System.out.println("You have successfully logged out");
-					logout = true;
-					currentUser = null;
-				}
-			}
+			currentUser.displayOptions();
+			System.out.println(); //for visibility
+			System.out.println("Press enter to continue, type /end to end");
+			String check = scan.nextLine();
+			if(check.equals("/end")) {end = true;}
+		}
+		
+		// TODO update csv
 	}
 }
