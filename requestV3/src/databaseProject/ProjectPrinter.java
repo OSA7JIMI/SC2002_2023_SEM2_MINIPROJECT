b16f723a5125ab941b601ebf4f71c3cd3dfbcd8a
PRYtheSheep <122869360+PRYtheSheep@users.noreply.github.com>
2023-04-01 09:30:00 +0800

    Update ProjectPrinter.java

--- requestV3/src/databaseProject/ProjectPrinter.java
+++ requestV3/src/databaseProject/ProjectPrinter.java
@@ -11,4 +11,37 @@ public class ProjectPrinter {
 		System.out.println("Supervisor : "+p.getSupervisor());
 		System.out.println("Status : "+p.getStatus());
 	}
+	
+	public static void printAllAvailable() {
+		for(int i=0; i<ProjectArray.size; i++) {
+			Project p = DatabaseProjectAccessor.getProject(i);
+			if(p.getStatus()==0) {
+				System.out.println("ID : "+p.getID());
+				System.out.println("Title : "+p.getTitle());
+				System.out.println("Status : "+p.getStatus());
+			}
+		}
+	}
+	
+	public static void printAllBasedOnSupervsior(int userID) {
+		for(int i=0; i<ProjectArray.size; i++) {
+			Project p = DatabaseProjectAccessor.getProject(i);
+			if(p.getID()==userID) {
+				System.out.println("ID : "+p.getID());
+				System.out.println("Title : "+p.getTitle());
+				System.out.println("Status : "+p.getStatus());
+			}
+		}
+	}
+	
+	public static void printAllBasedOnStatus(int status) {
+		for(int i=0; i<ProjectArray.size; i++) {
+			Project p = DatabaseProjectAccessor.getProject(i);
+			if(p.getStatus()==status) {
+				System.out.println("ID : "+p.getID());
+				System.out.println("Title : "+p.getTitle());
+				System.out.println("Status : "+p.getStatus());
+			}
+		}
+	}
 }
