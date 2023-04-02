bfa167f6dab7e20ee88a7bf9dee892d5b5953606
Tejaswi002 <117284370+Tejaswi002@users.noreply.github.com>
2023-04-02 18:09:36 +0800

    Add files via upload

--- 2002ASSIGNMENT/src/Request/Request.java
+++ 2002ASSIGNMENT/src/Request/Request.java
@@ -11,6 +11,7 @@ public class Request {
 	public String receiverID;
 	public boolean pending;
 	public boolean approval;
+	public boolean dblapproval = true; // added this
 	//0 for changeTitle, 1 for Allocate, 2 for De-register, 3 for Transfer
 	public int type;
 	//contains the index position in the sender's outgoing request array
