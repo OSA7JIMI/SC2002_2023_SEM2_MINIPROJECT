7df956aff59067bd3d445663b22eeb55b0c4a829
Tejaswi002 <117284370+Tejaswi002@users.noreply.github.com>
2023-04-07 23:18:58 +0800

    Update Request.java

--- Assignment2002/src/Request/Request.java
+++ Assignment2002/src/Request/Request.java
@@ -6,18 +6,89 @@ import Test.User;
 
 public abstract class Request {
 	
-	public int requestIndex;
-	public String senderID;
-	public String receiverID;
-	public boolean pending;
-	public boolean approval;
-	public boolean dblapproval = true; // added this
+	private int requestIndex;
+	private String senderID;
+	private String receiverID;
+	private boolean pending;
+	private boolean approval;
+	private boolean dblapproval = true; // added this
 	//0 for changeTitle, 1 for Allocate, 2 for De-register, 3 for Transfer
-	public int type;
+	private int type;
 	//contains the index position in the sender's outgoing request array
-	public String Title;
-	public int projectID = -1;
-	public String replacementID;
+	private String Title;
+	private int projectID = -1;
+	private String replacementID;
 	
 	public abstract void settleRequest(boolean approval);
+
+	public void setrequestIndex(int index) {
+		requestIndex = index;
+	}
+	public int getrequestIndex() {
+		return requestIndex;
+	}
+
+	public void setsenderID(String sendID) {
+		senderID = sendID;
+	}
+	public String getsenderID() {
+		return senderID;
+	}
+
+	public void setreceiverID(String receiveID) {
+		receiverID = receiveID;
+	}
+	public String getreceiverID() {
+		return receiverID;
+	}
+
+	public void setpending(boolean pending_) {
+		pending = pending_;
+	}
+	public boolean getpending() {
+		return pending;
+	}
+
+	public void setapproval(boolean approval_) {
+		approval = approval_;
+	}
+	public boolean getapproval() {
+		return approval;
+	}
+
+	public void setdblapproval(boolean dblapproval_) {
+		dblapproval = dblapproval_;
+	}
+	public boolean getdblapproval() {
+		return dblapproval;
+	}
+
+	public void settype(int typ) {
+		type = typ;
+	}
+	public int gettype() {
+		return type;
+	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setprojectID(int projID) {
+		projectID = projID;
+	}
+	public int getprojectID() {
+		return projectID;
+	}
+
+	public void setreplacementID(String replaceID) {
+		replacementID = replaceID;
+	}
+	public String getreplacementID() {
+		return replacementID;
+	}
+
 }
