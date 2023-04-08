1b60bc77b4ec690a768a5c9b1a60b062c3d2815a
OSA7JIMI <126651855+OSA7JIMI@users.noreply.github.com>
2023-04-08 15:26:04 +0800

    Update User.java

--- sc2002Project/src/Test/User.java
+++ sc2002Project/src/Test/User.java
@@ -44,7 +44,7 @@ public abstract class User {
 		Scanner a = new Scanner(System.in);
 		System.out.println("Please enter your new password: ");
 		password = a.nextLine();
-		System.out.println("Password changed succesfullyPlease log in again");
+		System.out.println("Password changed succesfully! Please log in again");
 	}	
 	
 	public abstract void viewAllProjects();
