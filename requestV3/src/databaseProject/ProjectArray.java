a0232ceae5d8f2043af194f3aef05a6436a4f9a3
OSA7JIMI <126651855+OSA7JIMI@users.noreply.github.com>
2023-04-10 13:53:35 +0800

    Delete requestV3/src directory

--- requestV3/src/databaseProject/ProjectArray.java
+++
@@ -1,9 +0,0 @@
-package databaseProject;
-
-import java.util.ArrayList;
-import test.Project;
-
-public class ProjectArray {
-	static ArrayList<Project> projectArray = new ArrayList<Project>();
-	static int size = 0;
-}
